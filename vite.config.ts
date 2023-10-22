8d714b5eadbcd7ada2a45a4b583c01072b89ff1b
joonas175 <salojarvi.joonas@gmail.com>
2023-10-22 12:55:58 +0300

    Fix base URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: 'https://github.com/joonas175/FoodoraLaskuri/',
+  base: 'https://joonas175.github.io/FoodoraLaskuri/',
 })
